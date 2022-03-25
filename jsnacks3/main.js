09f0b130cecc44ada8d50e0d4a46cae0d8fcb376
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-25 15:05:43 +0100

    esercizio3

---
+++ jsnacks3/main.js
@@ -0,0 +1,13 @@
+let numero1 = parseInt(prompt("inserisci un numero"));
+let numero2 = parseInt(prompt("inserisci un numero"));
+let numero3 = parseInt(prompt("inserisci un numero"));
+let numero4 = parseInt(prompt("inserisci un numero"));
+let numero5 = parseInt(prompt("inserisci un numero"));
+let numero6 = parseInt(prompt("inserisci un numero"));
+let numero7 = parseInt(prompt("inserisci un numero"));
+let numero8 = parseInt(prompt("inserisci un numero"));
+let numero9 = parseInt(prompt("inserisci un numero"));
+let numero10 = parseInt(prompt("inserisci un numero"));
+
+let somma = numero1 + numero2 + numero3 + numero4 + numero4 + numero6 + numero7 + numero8 + numero9 + numero10;
+console.log(somma);
--- jsnacks3/style.js
+++
@@ -1,7 +0,0 @@
-let
-
-for(let i= 0; i <=10; i++){
-    let numeri = prompt("inserisci un numero");
-    let somma = numero[i]
-
-}
