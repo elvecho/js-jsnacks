3f8b2c63548eed80c0758ed1f52fabbc9f5a4770
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-25 18:45:03 +0100

    esercizio 7

---
+++ jsnacks7/main.js
@@ -0,0 +1,13 @@
+let numeroUtente = parseInt(prompt("inserisci un numero"));
+console.log(numeroUtente);
+
+let numeroUtenteAlCubo = cubodeinumeri();
+console.log(numeroUtenteAlCubo);
+
+ function cubodeinumeri(addendo1) {
+   let addendo1 = numeroUtente;
+   let numeroUtente = Math.pow(numeroUtente,3);
+   let risultato = numeroUtente;
+     return risultato;
+     
+ }
