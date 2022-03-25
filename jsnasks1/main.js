c7c7d55124c168e204da591dfe72976f1e4b8e5d
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-25 14:22:27 +0100

    primo esercizio

---
+++ jsnasks1/main.js
@@ -0,0 +1,13 @@
+let numero1 = parseInt(prompt("inserisci il primo numero"));
+let numero2 = parseInt(prompt("inserisci il secondo numero"));
+
+console.log(numero1);
+console.log(numero2);
+
+if(numero1 > numero2){
+    alert("il primo numero è maggiore");
+    console.log("il primo numero è maggiore");
+}else{
+    alert("il secondo numero è maggiore");
+    console.log("il secondo numero è maggiore");
+}
