037effaa2cee413621496e739d2f54d51bec820a
elvecho <100934190+elvecho@users.noreply.github.com>
2022-03-26 16:44:38 +0100

    esercizio8

---
+++ jsnacks8/main.js
@@ -0,0 +1,14 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+somma= sumSimpleArray(array)
+console.log(somma);
+
+function sumSimpleArray(array) {
+    
+    array= array;
+    let sum = 0;
+    for(let i = 0; i < array.length; i++) {
+         sum += array[i];
+    }
+    return sum;
+}
